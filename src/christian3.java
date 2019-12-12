591ba2e77c27edcfb2f5312645bfab1952a06d76
ChristianCrousser <christian.crousserkaiman@gmail.com>
2019-12-12 14:28:34 -0600

    cleanup code exercise 3

--- src/christian3.java
+++ src/christian3.java
@@ -7,41 +7,39 @@ public class christian3 {
 
     public static void main(String[] args){
 
-
-
         System.out.println("Hello and welcome to my number guessing game.");
         System.out.println("Pick a number: ");
 
-        Scanner inputnum = new Scanner(System.in);  //This number will be the max number the player has to guess too.
+        Scanner inputnum = new Scanner(System.in);
         int maxnum;
         maxnum = inputnum.nextInt();
 
         Random rand = new Random();
         int number = rand.nextInt(maxnum);
-        int tries = 0; //Will increase depending on how many tries it takes
+        int tries = 0;
         Scanner input = new Scanner(System.in);
         int guess;
         boolean win = false;
 
-        while (win == false){  //This while loop false the code with in it repeat until win === true
+        while (win == false){
 
             System.out.println("Guess a number between 1 and "+ maxnum +": ");
             guess = input.nextInt();
-            tries++; //Increasing the number set in the variable tries by 1
+            tries++;
 
             if (guess == number){
-                win = true; //Since the number is correct win == true then ending the loop
-                //First thing the guess is compared too
+                win = true;
+
             }
 
             else if(guess < number){
                 System.out.println("Number is to low, tray again");
-                //2nd thing guess is compared too.
+
             }
 
             else if(guess > number){
                 System.out.println("Number is to high, try again");
-                //3rd thing guess is compared too.
+
             }
 
         }
